dee6417007bbf246c60a70ff16137e4691b76a48
azin1129 <nuestiloveyou@gmail.com>
2023-12-19 08:40:41 +0900

    @Bean을 사용한 CommandLineRunner 정의

--- ch2-3/src/main/java/com/example/demo/Ch23Application.java
+++ ch2-3/src/main/java/com/example/demo/Ch23Application.java
@@ -5,17 +5,25 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Ch23Application implements CommandLineRunner{
+public class Ch23Application{
 	protected final Log logger=LogFactory.getLog(getClass());
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Ch23Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception{
-		logger.info("CourseTrackerApplication CommandLineRunner  has executed");
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args->{
+			logger.info("CommandLineRunner executed as a bean definition with "
+					+ args.length+" arguments");
+			
+			for(int i=0;i<args.length;i++) {
+				logger.info("Argument:"+args[i]);
+			}
+		};
 	}
 }
