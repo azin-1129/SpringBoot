780f99f7f330b161ab82a4bce8752fd1a83eb971
azin1129 <nuestiloveyou@gmail.com>
2023-12-20 21:17:12 +0900

    :rotating_light: Custom bean validation annotation. 실행이 안 된다!!!!!!

---
+++ ch2-5-2/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import com.example.demo.validation.Password;
+
+public class User {
+
+    private String userName;
+
+    @Password
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
